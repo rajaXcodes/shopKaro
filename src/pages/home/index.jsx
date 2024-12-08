8c6e099540089b0894f40cc056bf412bbd5f83eb
rajaXcodes <konig.9122005@gmail.com>
2024-12-08 11:55:00 +0530

    added the home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 import { useNavigate } from "react-router-dom";
 function Home() {
   const navigate = useNavigate();
   return (
     <section className="hero overflow-hidden ">
-    <div className="hero-content">
-      <h1 className="hero-title">Exclusive Summer Sale!</h1>
-      <p className="hero-subtitle">Up to 50% off on all products.</p>
-      <button className="hero-btn"onClick={()=>navigate('/product-list')}>Shop Now</button>
-    </div>
-  </section>
-  )
+      <div className="hero-content">
+        <h1 className="hero-title">Exclusive Summer Sale!</h1>
+        <p className="hero-subtitle">Up to 50% off on all products.</p>
+        <button className="hero-btn" onClick={() => navigate("/product-list")}>
+          Shop Now
+        </button>
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
